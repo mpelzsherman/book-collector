04b84706fb914aaf00ab2472f8e7a389e76dc351
Michael Pelz-Sherman <msherman@federatedmedia.net>
2012-04-27 15:25:54 -0400

    fixed book ownership issue but have_css still not working!

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -7,7 +7,9 @@ describe 'home page' do
     it "should display my list of books" do
       user = FactoryGirl.create(:user)
       login_as(user, :scope => :user)
-      FactoryGirl.create(:book)
+      book = FactoryGirl.create(:book)
+      book.user = user
+      book.save
       visit root_path
       save_and_open_page
       page.should have_css('div.booklist')
