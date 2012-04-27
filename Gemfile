3089d47cab5001969d7c2db9b98976d0a17d456f
Michael Pelz-Sherman <msherman@federatedmedia.net>
2012-04-26 23:16:50 -0400

    Added Books entity & migration. Added haml-rails gem and haml code for home page.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'mysql2'
 
 gem 'devise'
 
+gem 'haml-rails'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420225901) do
+ActiveRecord::Schema.define(:version => 20120427023615) do
+
+  create_table "books", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
